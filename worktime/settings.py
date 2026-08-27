2022a786732aa981209c8ae6c907215591f1f349
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2026-08-27 12:48:13 +0200

    Connect Neon Postgres database

--- worktime/settings.py
+++ worktime/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -65,10 +66,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'worktime.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []

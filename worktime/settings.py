df5184cd63bfacb3a31b9b5d9e0a4c7d5e7d09da
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2026-08-27 13:59:40 +0200

    add static files

--- worktime/settings.py
+++ worktime/settings.py
@@ -92,6 +92,8 @@ LOCALE_PATHS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

aac2103e19be281e5b5660f6188ff842e739b0d2
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2026-08-28 10:30:21 +0200

    Add createsuperuser step

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear --verbosity 1
 echo "==> Static files collected: $(find staticfiles -type f 2>/dev/null | wc -l) files"
 python manage.py migrate
+python manage.py createsuperuser --noinput || true

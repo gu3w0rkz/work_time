ffb30df02cd76b3674de4f0e92e180dde4a82f6e
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2026-08-28 10:12:37 +0200

    Fix build.sh: remove incorrect cd api

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-cd api
 python manage.py collectstatic --no-input --clear --verbosity 1
 echo "==> Static files collected: $(find staticfiles -type f 2>/dev/null | wc -l) files"
 python manage.py migrate
